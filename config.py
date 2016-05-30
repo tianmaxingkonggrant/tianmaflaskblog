a6bfaed68ffd89c43d57b7e59f0534b292693eea
wanglianjun <git@wanglianjun.com>
2016-05-30 11:47:57 +0800

    basicConfiguration

--- config.py
+++ config.py
@@ -1,1 +1,49 @@
 # coding=utf-8
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard guess'
+	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+	FLASK_MAIL_SUBJECT_PREFIX = '[tianmaflaskblog]'
+	FLASK_MAIL_SENDER = os.environ.get('FLASK_ADMIN') or '2550555830@qq.com'
+	FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+
+	@staticmethod
+	def init_app(app):
+		pass
+
+class DevelopmentConfig(Config):
+	DEBUG = True
+	MAIL_SERVER = 'smtp.qq.com'
+	MAIL_PORT = '587'
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+	TESTING = True
+	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI') or \
+		'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
+		'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+	'development': DevelopmentConfig,
+	'testing': TestingConfig,
+	'production': ProductionConfig,
+	'default': DevelopmentConfig
+}
+
+
+
+
+
