a297b5923fcc3ebc874a2195dcc9301cd09f4764
wanglianjun <git@wanglianjun.com>
2016-06-03 10:23:26 +0800

    moderate-comment

--- app/main/views.py
+++ app/main/views.py
@@ -189,9 +189,49 @@ def show_all():
 	resp.set_cookie('show_followed', '', max_age=30 * 24 * 60 * 60)
 	return resp
 
+
 @main.route('/followed')
 def show_followed():
 	resp = make_response(redirect(url_for('main.index')))
 	resp.set_cookie('show_followed', '1', max_age=30 * 24 * 60 * 60)
 	return resp
+
+
+@main.route('/moderate')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate():
+	page = request.args.get('page', 1, type=int)
+	pagination =  Comment.query.order_by(Comment.timestamp.desc()).paginate(
+		page, per_page=current_app.config['FLASK_PER_PAGE_COMMENT'], error_out=\
+		False
+	)
+	comments = pagination.items
+	return render_template('moderate.html', comments=comments, pagination=pagination,
+						   page=page)
+
+
+
+@main.route('/moderate/enable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_enable(id):
+	comment = Comment.query.get_or_404(id)
+	comment.disabled = False
+	db.session.add(comment)
+	return redirect(url_for('main.moderate', page=request.args.get('page', 1, type=int)))
+
+@main.route('/moderate/disable/<int:id>')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def moderate_disabled(id):
+	comment = Comment.query.get_or_404(id)
+	comment.disabled =True
+	db.session.add(comment)
+	return redirect(url_for('main.moderate',page=request.args.get('page', 1, type=int)))
+
+
+
+
+
 
